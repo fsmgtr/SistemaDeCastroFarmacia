a13bbd7f5a519b561b8ac3d0bc244715b90cf03d
fsmgtr <80686808+fsmgtr@users.noreply.github.com>
2021-03-22 21:45:35 -0300

    Delete Farmacia directory

--- Farmacia/WebContent/resources/js/script.js
+++
@@ -1,9 +0,0 @@
-function verificar(xhr, status, args, dlg, tb) {
-        if(args.validationFailed) {
-            PF(dlg).jq.effect("shake", {times:5}, 100);
-        }
-        else {
-            PF(dlg).hide();
-            PF(tb).clearFilters();
-        }
-    }
